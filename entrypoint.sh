fa0dd9cacb98dc4735033fa92719ede5f42aad66
Jeff Triplett <jeff.triplett@gmail.com>
2019-01-28 13:16:11 -0600

    :tractor: Updates python name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if ! [ -e "${GITHUB_WORKSPACE}/venv/bin/activate" ]; then
-    python3 -m venv "${GITHUB_WORKSPACE}/venv/"
+    python -m venv "${GITHUB_WORKSPACE}/venv/"
 fi
 
 source "${GITHUB_WORKSPACE}/venv/bin/activate"
